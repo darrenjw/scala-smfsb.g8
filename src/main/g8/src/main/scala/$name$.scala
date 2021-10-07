658d4a0ee9c97eca4d3a5683495279c400a2216a
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2021-10-07 21:52:16 +0100

    syntax update

--- src/main/g8/src/main/scala/$name$.scala
+++ src/main/g8/src/main/scala/$name$.scala
@@ -2,17 +2,13 @@
 Stub for scala-smfsb code
 */
 
-object Stub {
+import smfsb.*
+import breeze.linalg.*
+import breeze.numerics.*
 
-  import smfsb._
-  import breeze.linalg._
-  import breeze.numerics._
+@main def go() =
+  val model = SpnModels.lv[IntState]()
+  val step = Step.gillespie(model)
+  val ts = Sim.ts(DenseVector(50, 100), 0.0, 20.0, 0.05, step)
+  Sim.plotTs(ts, "Gillespie simulation of LV model")
 
-  def main(args: Array[String]): Unit = {
-    val model = SpnModels.lv[IntState]()
-    val step = Step.gillespie(model)
-    val ts = Sim.ts(DenseVector(50, 100), 0.0, 20.0, 0.05, step)
-    Sim.plotTs(ts, "Gillespie simulation of LV model")
-  }
-
-}
